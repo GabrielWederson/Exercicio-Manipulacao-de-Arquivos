3ee42f2093ef96600a0cd7a39cefb2aa7f76012a
Gabriel Wederson <gabrielwederson582@gmail.com>
2026-02-14 16:56:16 -0300

    adicao do metodo de somatorio de produtos

--- src/ArquivoService.java
+++ src/ArquivoService.java
@@ -1,2 +1,16 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
 public class ArquivoService {
+
+    String caminho = "C:\\Users\\gabri\\OneDrive\\Documentos\\FileReader-FileWriter\\FileReader-Writer\\csvs\\produto.csv";
+
+
+    public void somatoriaProddutos(){
+
+    }
+
+
 }
