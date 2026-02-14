796c67faa9cd79b945c82792cba901ca67993e83
Gabriel Wederson <gabrielwederson582@gmail.com>
2026-02-14 18:01:23 -0300

    criado metodo somatorioProdutos da classe ArquivoService

--- src/ArquivoService.java
+++ src/ArquivoService.java
@@ -1,14 +1,40 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 
 public class ArquivoService {
 
-    String caminho = "C:\\Users\\gabri\\OneDrive\\Documentos\\FileReader-FileWriter\\FileReader-Writer\\csvs\\produto.csv";
+    String caminho = "C:\\Users\\gabri\\OneDrive\\Documentos\\FileReader-FileWriter\\FileReader-Writer\\csvs";
+
+    public void somatoriaProddutos(Produto produto){
+
+        String linhaProduto = produto.getNome() + "," + produto.getValor() + "," + produto.getQuantidade();
+        String linhaSomatorio = produto.getNome() + "," + produto.getValor() * produto.getQuantidade();
+
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(caminho))){
+            boolean criado = new File(caminho + "\\saida").mkdir();
+
+        } catch(IOException e){
+            System.out.println("Erro: " + e.getMessage());
+        }
+
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(caminho + "\\produto.csv", true))){
+                bw.write(linhaProduto);
+                bw.newLine();
+
+            System.out.println("produto adicionado a arquivo produto.csv");
+        } catch(IOException e){
+            System.out.println("Erro: " + e.getMessage());
+        }
 
 
-    public void somatoriaProddutos(){
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(caminho + "\\saida\\somatorio.csv", true))){
+                bw.write(linhaSomatorio);
+                bw.newLine();
+
+            System.out.println("produto adicionado a arquivo somatorio.csv");
+        } catch(IOException e){
+            System.out.println("Erro: " + e.getMessage());
+        }
 
     }
 
