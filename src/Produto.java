14387bf6ff4e49a0b3963df07f931d93c9b08868
Gabriel Wederson <gabrielwederson582@gmail.com>
2026-02-14 16:15:12 -0300

    adicao gitattributes

--- src/Produto.java
+++ src/Produto.java
@@ -1,2 +1,35 @@
 public class Produto {
+    private String nome;
+    private double Valor;
+    private int quantidade;
+
+    public Produto(String nome, double valor, int quantidade) {
+        this.nome = nome;
+        this.Valor = valor;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return Valor;
+    }
+
+    public void setValor(double valor) {
+        Valor = valor;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
 }
